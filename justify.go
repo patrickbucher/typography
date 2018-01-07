b4271426e7cb6566e6c8496b0b662a01b52a84b3
patrickbucher <patrick.bucher@stud.hslu.ch>
2018-01-07 20:06:38 +0100

    first working version of justify

---
+++ justify.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"bufio"
+	"bytes"
+	"fmt"
+	"strings"
+)
+
+func main() {
+	text := "This is a test.\nAnd another test.\nAnd yet another test.\nOne more test.\nAnd yet another stupid test.\nAnd here's yet another incredibly stupid test."
+	length := Longest(strings.Split(text, "\n"))
+	justified := Justify(text, length)
+	fmt.Printf("%s\n", justified)
+}
+
+// Justify aligns the text to the left and right edge by filling the word gaps
+// up with spaces until length is reached.
+func Justify(text string, length int) string {
+	output := bytes.NewBufferString("")
+	reader := strings.NewReader(text)
+	scanner := bufio.NewScanner(reader)
+	scanner.Split(bufio.ScanLines)
+	for scanner.Scan() {
+		line := scanner.Text()
+		if len(line) > length {
+			// TODO client's problem?
+			continue
+		}
+		words := strings.Fields(line)
+		spaces := make([]string, len(words)-1)
+		diff := length - len(line) + len(spaces) // spaces where omitted, add them back
+		for d, i := diff, 0; d > 0; d-- {
+			spaces[i] += " "
+			i++
+			if i == len(spaces) {
+				i = 0
+			}
+		}
+		justified := bytes.NewBufferString("")
+		for w, s := 0, 0; w < len(words) || s < len(spaces); w, s = w+1, s+1 {
+			if w < len(words) {
+				justified.WriteString(words[w])
+			}
+			if s < len(spaces) {
+				justified.WriteString(spaces[s])
+			}
+		}
+		line = justified.String()
+		output.WriteString(line)
+		output.WriteRune('\n')
+	}
+	return output.String()
+}
+
+func Longest(lines []string) int {
+	var longest int
+	for _, v := range lines {
+		l := len(v)
+		if l > longest {
+			longest = l
+		}
+	}
+	return longest
+}
+
+func reverse(text *string) {
+	runes := []rune(*text)
+	for i, j := 0, len(runes)-1; i < j; i, j = i+1, j-1 {
+		runes[i], runes[j] = runes[j], runes[i]
+	}
+	*text = string(runes)
+}
